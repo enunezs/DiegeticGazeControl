9efaca7b051bfbfb818c029accfe0ab34af7656f
Emanuel Nunez <github@enunezs.com>
2024-03-07 21:11:55 +0000

    Launch file rewriten to load configs properly

--- src/diegetic_button_pkg/launch/eyes_to_joy.launch.py
+++ src/diegetic_button_pkg/launch/eyes_to_joy.launch.py
@@ -1,52 +1,73 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch.substitutions import ThisLaunchFileDir, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            Node(
-                package="pupil_neon_pkg",
-                executable="pupil_publisher.py",
-                name="pupil_glasses",
-                arguments=[("__log_level:=debug")],
-                output="screen",
-                parameters=["src/pupil_neon_pkg/config/params.yaml"],
-            ),
-            Node(
-                package="fiducials",
-                executable="aruco_detect.py",
-                name="aruco_detect",
-                parameters=["src/fiducials/config/params.yaml"],
-            ),
-            Node(
-                package="fiducials",
-                executable="aruco_mapper.py",
-                name="aruco_mapper",
-                parameters=[
-                    {
-                        "config_file_path": "src/fiducials/config/mapping.csv",
-                        "root_frame": "camera",
-                    }
-                ],
-            ),
-            Node(
-                package="diegetic_button_pkg",
-                executable="diegetic_button.py",
-                name="diegetic_button",
-                parameters=["src/diegetic_button_pkg/config/params.yaml"],
-            ),
-            Node(
-                package="diegetic_button_pkg",
-                executable="input_check.py",
-                name="input_check",
-                parameters=["src/diegetic_button_pkg/config/params.yaml"],
-            ),
-            Node(
-                package="diegetic_button_pkg",
-                executable="joy.py",
-                name="joy",
-            ),
-        ]
+    launch_description = LaunchDescription()
+
+    pupil_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("pupil_neon_pkg"), "launch"),
+                "/pupil.launch.py",
+            ]
+        )
     )
+    launch_description.add_action(pupil_launch)
+
+    # Fiducials
+    fiducials_node_detect = Node(
+        package="fiducials",
+        executable="aruco_detect.py",
+        name="aruco_detect",
+        parameters=["src/fiducials/config/params.yaml"],
+    )
+    launch_description.add_action(fiducials_node_detect)
+
+    fiducials_node_mapper = Node(
+        package="fiducials",
+        executable="aruco_mapper.py",
+        name="aruco_mapper",
+        parameters=[
+            {
+                "config_file_path": "src/fiducials/config/mapping.csv",
+                "root_frame": "camera",
+            }
+        ],
+    )
+    launch_description.add_action(fiducials_node_mapper)
+
+    # Diegetic Button
+    diegetic_button_node = Node(
+        package="diegetic_button_pkg",
+        executable="diegetic_button.py",
+        name="diegetic_button",
+        parameters=["src/diegetic_button_pkg/config/params.yaml"],
+    )
+    launch_description.add_action(diegetic_button_node)
+
+    input_check_node = Node(
+        package="diegetic_button_pkg",
+        executable="input_check.py",
+        name="input_check",
+        parameters=["src/diegetic_button_pkg/config/params.yaml"],
+    )
+    launch_description.add_action(input_check_node)
+
+    # Joy
+    joy_node = Node(
+        package="diegetic_button_pkg",
+        executable="joy.py",
+        name="joy",
+        parameters=["src/diegetic_button_pkg/config/params.yaml"],
+    )
+    launch_description.add_action(joy_node)
+
+    return launch_description
