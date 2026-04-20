0566c4917f5b3377e940d20c9bf702256d861594
Emanuel Nunez <github@enunezs.com>
2026-04-20 12:34:12 +0100

    Fixed cmake. Tests running and failing

--- src/gaze_interaction_manager/test/test_gaze_interpolation.py
+++ src/gaze_interaction_manager/test/test_gaze_interpolation.py
@@ -1,7 +1,7 @@
 import sys
 import os
 # Point to your 'src' folder so PyTest can import your nodes
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'scripts')))
 
 # Now you can import your classes! (Adjust the filename to match yours)
 from gaze_controller import GazeController
